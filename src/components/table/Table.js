5ca9e74995aa572bfa9d9d0f5d7edc38add89f29
Bilion <bilionjenia1111@gmail.com>
2025-03-26 16:39:30 +0100

    added logic on resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,19 +1,24 @@
-/* eslint-disable max-len */
 import { ExcelComponent } from '../../core/ExcelComponent';
+import { tableResize } from './table.resize';
 import { createTable } from './table.template';
 
 export class Table extends ExcelComponent {
 	static className = 'excel__table';
 	rowsCount = 20;
 	colsCount = 25;
+
 	constructor($root) {
 		super($root, {
-			name: 'Formula',
-			// listeners: ['input'],
+			name: 'Table',
+			listeners: ['mousedown'],
 		});
 	}
 
 	toHTML() {
 		return createTable(this.rowsCount, this.colsCount);
+	}
+
+	onMousedown(event) {
+		tableResize(event);
 	}
 }
